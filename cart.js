765d3cb77fa463f312d90c4ed792ea5fd0f8d6aa
VignesCoOl <79147341+VignesCoOl@users.noreply.github.com>
2021-02-23 13:06:26 +0530

    Update

--- cart.js
+++ cart.js
@@ -22,7 +22,7 @@ let products = [
         tag: 'mobile',
         price: 25000,
         inCart: 0
-    },       {
+    }, {
         name: 'OnePlus',
         tag: 'mobile',
         price: 39900,
@@ -37,10 +37,10 @@ for (let i = 0; i < carts.length; i++) {
         total(products[i]);
     })
 }
-function onLoadCart(){
+function onLoadCart() {
     let productnums = localStorage.getItem("cartnumbers");
-    if(productnums){
-        document.querySelector(".cart span").textContent= productnums;
+    if (productnums) {
+        document.querySelector(".cart span").textContent = productnums;
     }
 }
 function cartNumbers(product) {
@@ -48,43 +48,60 @@ function cartNumbers(product) {
     productnums = parseInt(productnums);
     if (productnums) {
         localStorage.setItem("cartnumbers", productnums + 1);
-        document.querySelector(".cart span").textContent= productnums+1;
+        document.querySelector(".cart span").textContent = productnums + 1;
     }
     else {
         localStorage.setItem("cartnumbers", 1);
-        document.querySelector(".cart span").textContent=1;
+        document.querySelector(".cart span").textContent = 1;
     }
     setItems(product);
 }
-function total(product){
+function total(product) {
     let carttotal = localStorage.getItem("total");
-    
-    console.log("Cart Total is" , carttotal);
+    console.log("Cart Total is", carttotal);
     console.log(typeof carttotal);
-    if(carttotal!=null){
+    if (carttotal != null) {
         carttotal = parseInt(carttotal);
         localStorage.setItem("total", carttotal + product.price);
     }
-    else{
+    else {
         localStorage.setItem("total", product.price);
     }
-    
 }
-function setItems(product){
+function setItems(product) {
     let cartItems = localStorage.getItem("productsIncart");
     cartItems = JSON.parse(cartItems);
-    if(cartItems != null){
-        if(cartItems[product.tag]==undefined){
+    if (cartItems != null) {
+        if (cartItems[product.tag] == undefined) {
             cartItems = {
-                ...cartItems, [product.tag]:product
+                ...cartItems, [product.tag]: product
             }
         }
         cartItems[product.tag].inCart += 1;
     }
-    else{
+    else {
         product.inCart = 1;
-        cartItems={[product.tag]:product}
+        cartItems = { [product.tag]: product }
     }
     localStorage.setItem("productsIncart", JSON.stringify(cartItems));
 }
+function displayCart() {
+let cartItems = localStorage.getItem("productsIncart");
+cartItems = JSON.parse(cartItems);
+let productcon = document.querySelector(".product-container");
+if(cartItems && productcon){
+productcon.innerHTML = "";
+Object.values(cartItems).map(item =>{
+productcon.innerHTML += <div class="product">
+<img src="${item.tag}.jpg"></img>
+<span>${item.name}</span>
+</div>
+
+
+})
+}
+
+}
+
 onLoadCart();
+displayCart();
